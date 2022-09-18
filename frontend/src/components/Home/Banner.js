a592cf424b127233f3b0ca7ad4b9900afbf54f8c
André Paolino <andre.senechal@hotmail.com>
2022-09-18 18:31:26 +0100

    fix linter problems

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,11 +1,11 @@
-import React,  { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import agent from "../../agent";
 import logo from "../../imgs/logo.png";
 import { connect } from "react-redux";
 import { CHANGE_TAB } from "../../constants/actionTypes";
 
 const isFilter = (value) => value.length >= 3;
-const isAll = (value, isSearching) => value.length === 2 && isSearching
+const isAll = (value, isSearching) => value.length === 2 && isSearching;
 
 const mapDispatchToProps = (dispatch) => ({
   onSearch: (tab, pager, payload) =>
@@ -13,31 +13,33 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const mapStateToProps = (state) => ({
-  tab: state.itemList?.tab
+  tab: state.itemList?.tab,
 });
 
 const Banner = ({ tab, onSearch }) => {
-
   const [search, setSearch] = useState("");
   const [isSearching, setSearching] = useState(false);
   useEffect(() => {
     setSearch("");
-  }, [tab])
-
+  }, [tab]);
 
   const handleSearchChange = (event) => {
     const value = event.target.value;
     setSearch(event.target.value);
 
-    if(isAll(value, isSearching)) {
-      onSearch(tab, agent.Items.all,  agent.Items.all());
+    if (isAll(value, isSearching)) {
+      onSearch(tab, agent.Items.all, agent.Items.all());
       setSearching(false);
     }
-    if(isFilter(value)) {
-      onSearch(tab, () => agent.Items.byTitle(value),  agent.Items.byTitle(value));
+    if (isFilter(value)) {
+      onSearch(
+        tab,
+        () => agent.Items.byTitle(value),
+        agent.Items.byTitle(value)
+      );
       setSearching(true);
     }
-  }
+  };
 
   return (
     <div className="banner text-white">
@@ -48,16 +50,16 @@ const Banner = ({ tab, onSearch }) => {
             <span id="get-part">A place to get</span>
           </div>
           <div className="col-4 px-1">
-          <i className="bi-search banner-search-form-icon" >
-            <input
-              id="search-box"
-              className="form-control form-control"
-              type="text"
-              placeholder="What is that you truly desire?"
-              value={search}
-              onChange={handleSearchChange}
-            />
-          </i>
+            <i className="bi-search banner-search-form-icon">
+              <input
+                id="search-box"
+                className="form-control form-control"
+                type="text"
+                placeholder="What is that you truly desire?"
+                value={search}
+                onChange={handleSearchChange}
+              />
+            </i>
           </div>
           <div className="col-auto px-1">
             <span> the cool stuff.</span>
